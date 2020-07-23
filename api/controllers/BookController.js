332a5d08c5727e534f3d44da66011776bc50b58b
Lucian Ghimfus <lucian.ghimfus@gmail.com>
2020-07-23 18:05:00 +0300

    implement update book by id path

--- api/controllers/BookController.js
+++ api/controllers/BookController.js
@@ -6,17 +6,17 @@
  */
 
 module.exports = {
-  createBook: function (req, res) {
+  addOne: function (req, res) {
     Book.create(req.body).exec(function (err) {
       if (err) {
         return res.status(400).json({ success: false, message: err.details });
       }
 
-      return res.status(200).json({ success: true });
+      return res.status(201).json({ success: true });
     });
   },
 
-  getBooks: function (req, res) {
+  getAll: function (req, res) {
     Book.find().exec(function (err, books) {
       if (err) {
         return res.status(400).json({ success: false, message: err.details });
@@ -26,7 +26,7 @@ module.exports = {
     });
   },
 
-  getBookById: function (req, res) {
+  getById: function (req, res) {
     Book.findOne({ id: req.param("id") }).exec(function (err, book) {
       if (err) {
         return res.status(400).json({ success: false, message: err.details });
@@ -34,5 +34,24 @@ module.exports = {
 
       return res.status(200).json(book);
     });
+  },
+
+  updateOne: async function (req, res) {
+    var updatedBook;
+
+    try {
+      updatedBook = await Book.updateOne({ id: req.param("id") }).set(req.body);
+    } catch (err) {
+      return res.status(400).json({ success: false, message: err.details });
+    }
+
+    if (updatedBook) {
+      return res.status(204).json(updatedBook);
+    } else {
+      return res.status(404).json({
+        success: false,
+        message: "Could not find a book for the specified id.",
+      });
+    }
   },
 };
--- test/integration/controllers/BookController.test.js
+++ test/integration/controllers/BookController.test.js
@@ -1,8 +1,8 @@
 var supertest = require("supertest");
 const chai = require("chai");
 
-describe("BookController.createBook", () => {
-  describe("#create()", function () {
+describe("BookController.addOne", () => {
+  describe("#addOne()", function () {
     it("should create a new book", function (done) {
       supertest(sails.hooks.http.app)
         .post("/api/create")
@@ -17,7 +17,7 @@ describe("BookController.createBook", () => {
         )
         .set("Content-Type", "application/json")
         .set("Accept", "application/json")
-        .expect(200)
+        .expect(201)
         .end(function (err, res) {
           if (err) {
             throw err;
@@ -28,8 +28,8 @@ describe("BookController.createBook", () => {
   });
 });
 
-describe("BookController.getBooks", () => {
-  describe("#get()", function () {
+describe("BookController.getAll", () => {
+  describe("#getAll()", function () {
     it("should get all the books", function (done) {
       supertest(sails.hooks.http.app)
         .get("/api/get/all")
@@ -45,13 +45,40 @@ describe("BookController.getBooks", () => {
   });
 });
 
-describe("BookController.getBooks", () => {
-  describe("#get()", function () {
+describe("BookController.getById", () => {
+  describe("#getById()", function () {
     it("should get one book by id", function (done) {
       supertest(sails.hooks.http.app)
-        .get("/api/get/1")
+        .get("/api/get/:id")
         .set("Accept", "application/json")
         .expect(200)
+        .end(function (err, res) {
+          if (err) {
+            throw err;
+          }
+          done();
+        });
+    });
+  });
+});
+
+describe("BookController.updateOne", () => {
+  describe("#updateOne()", function () {
+    it("should update book data", function (done) {
+      supertest(sails.hooks.http.app)
+        .post("/api/update/:id")
+        .send(
+          JSON.stringify({
+            title: "The hitchhiker's guide",
+            author: "Douglas Adams",
+            year: 2010,
+            description:
+              "This is just a description of hitchhiker's guide to the galaxy by Douglas Adams",
+          })
+        )
+        .set("Content-Type", "application/json")
+        .set("Accept", "application/json")
+        .expect(204)
         .end(function (err, res) {
           if (err) {
             throw err;
